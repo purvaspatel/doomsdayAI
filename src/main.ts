315ee3c28149e3a58f0077ae52f659e29e3c4925
unknown <purvaspatel1241@gmail.com>
2026-02-04 22:14:09 +0530

    my first commir

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>DoomsdayAI</h1>
-  </div>
+<article>
+<h1>DoomsdayAI</h1>
+<p>Welcom to DoomsdayAI</p>
+<p>A project by Purva & Karan</p>
+</article>
 `
 console.log('DoomsdayAI initialized');
